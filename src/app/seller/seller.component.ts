c7f7c318cba2fcb510a625d14c01e84d6a076687
fullystakd <fullystakd@gmail.com>
2021-10-08 19:19:37 -0700

    created and rendered seller component

---
+++ src/app/seller/seller.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller',
+  templateUrl: './seller.component.html',
+  styleUrls: ['./seller.component.css']
+})
+export class SellerComponent implements OnInit {
+
+  sellers:string[];
+  
+  constructor() {
+    this.sellers = ["BestBuy","Apple","Amazon"]
+   }
+
+   getSellers():string[]{
+     return this.sellers;
+   }
+
+  ngOnInit(): void {
+  }
+
+}
